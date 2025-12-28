ae569f6787ef16ca87be2a3185e79178e2ce34a2
MohamadAminGhanoum <mohamad.amin.ghanoum@gmail.com>
2025-12-28 23:06:20 +0100

    This was sooo annoying, coding js is really difficult for me right now, since im beginner at js, so im experimenting with it thats main reasonf ro soo long time

--- saveItems.js
+++ saveItems.js
@@ -3,7 +3,7 @@ function saveItem(){
         const code = document.getElementById("codeInput").value;
 
         const items=JSON.parse(localStorage.getItem("items"))|| [];
-        items.push({name});
+        items.push({name, code});
         localStorage.setItem("items",JSON.stringify(items));
         alert("Project saved");
         showProject(name,code);
@@ -11,4 +11,40 @@ function saveItem(){
 
     function showProject(name, code){
         const container=document.getElementById("projectLists");
+        const box=document.createElement("button")
+        box.textContent=name;
+            const getrid =document.createElement("div");
+            getrid.className = "removeProject";
+        box.onclick=function() {
+            document.getElementById("projectsSide").textContent = code;
+        };
+
+const deletebutton = document.createElement("img")
+deletebutton.src= "delete.png";
+deletebutton.className="deleteproject";
+deletebutton.onclick = function() {
+    if (confirm('Delete ${name}?')){
+        removeProjectFromStorage(name);
+        getrid.remove();
     }
+};
+getrid.appendChild(box);
+getrid.appendChild(deletebutton);
+container.appendChild(getrid)
+    }
+
+    window.onload=function() {
+        const items = JSON.parse(localStorage.getItem("items")) || [];
+
+        items.forEach(function(project) {
+            showProject(project.name, project.code);
+        });
+
+    };
+
+
+function removeProjectFromStorage(nameToRemove) {
+    let items = JSON.parse(localStorage.getItem("items")) || [];
+    items = items.filter(project => project.name !== nameToRemove);
+    localStorage.setItem("items", JSON.stringify(items));
+}
